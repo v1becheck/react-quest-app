8cadeafe1102d7aee6360fd47ecf5ca4eb31c68e
Milos Ilic <milosilic89@outlook.com>
2024-01-09 16:09:38 +0100

    useReducer switch

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,25 +1,46 @@
-import { useState } from "react";
+import { useReducer, useState } from 'react';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'inc':
+      return state + 1;
+    case 'dec':
+      return state - 1;
+    case 'setCount':
+      return action.payload;
+    case 'defineStep':
+      return action.payload;
+    default:
+      throw new Error();
+  }
+}
 
 function DateCounter() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
+  const [count, dispatch] = useReducer(reducer, 0);
+
   const [step, setStep] = useState(1);
 
   // This mutates the date object.
-  const date = new Date("june 21 2027");
+  const date = new Date('june 21 2027');
   date.setDate(date.getDate() + count);
 
   const dec = function () {
+    dispatch({ type: 'dec' });
+
     // setCount((count) => count - 1);
-    setCount((count) => count - step);
+    // setCount((count) => count - step);
   };
 
   const inc = function () {
+    dispatch({ type: 'inc' });
     // setCount((count) => count + 1);
-    setCount((count) => count + step);
+    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
-    setCount(Number(e.target.value));
+    dispatch({ type: 'setCount', payload: Number(e.target.value) });
+    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
@@ -27,17 +48,17 @@ function DateCounter() {
   };
 
   const reset = function () {
-    setCount(0);
+    // setCount(0);
     setStep(1);
   };
 
   return (
-    <div className="counter">
+    <div className='counter'>
       <div>
         <input
-          type="range"
-          min="0"
-          max="10"
+          type='range'
+          min='0'
+          max='10'
           value={step}
           onChange={defineStep}
         />
@@ -58,4 +79,5 @@ function DateCounter() {
     </div>
   );
 }
+
 export default DateCounter;
