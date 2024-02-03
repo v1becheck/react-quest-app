ff011a70a96ef139eb059bc6326675b6ecff220f
Milos Ilic <milosilic89@outlook.com>
2024-02-03 09:58:39 +0100

    Managing Related Pieces of State

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,25 +1,27 @@
-import { useReducer, useState } from 'react';
+import { useReducer } from 'react';
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
   switch (action.type) {
     case 'inc':
-      return state + 1;
+      return { ...state, count: state.count + state.step };
     case 'dec':
-      return state - 1;
+      return { ...state, count: state.count - state.step };
     case 'setCount':
-      return action.payload;
-    case 'defineStep':
-      return action.payload;
+      return { ...state, count: action.payload };
+    case 'setStep':
+      return { ...state, step: action.payload };
+    case 'reset':
+      return initialState;
     default:
-      throw new Error();
+      throw new Error('Unknown action type');
   }
 }
 
 function DateCounter() {
-  // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
-
-  const [step, setStep] = useState(1);
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date('june 21 2027');
@@ -27,29 +29,22 @@ function DateCounter() {
 
   const dec = function () {
     dispatch({ type: 'dec' });
-
-    // setCount((count) => count - 1);
-    // setCount((count) => count - step);
   };
 
   const inc = function () {
     dispatch({ type: 'inc' });
-    // setCount((count) => count + 1);
-    // setCount((count) => count + step);
   };
 
   const defineCount = function (e) {
     dispatch({ type: 'setCount', payload: Number(e.target.value) });
-    // setCount(Number(e.target.value));
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    dispatch({ type: 'setStep', payload: Number(e.target.value) });
   };
 
   const reset = function () {
-    // setCount(0);
-    setStep(1);
+    dispatch({ type: 'reset' });
   };
 
   return (
