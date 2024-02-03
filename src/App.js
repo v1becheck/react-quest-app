5b488c2c20c789576f8fcaeadaef069832b0f35c
Milos Ilic <milosilic89@outlook.com>
2024-02-03 11:57:56 +0100

    Loading Questions from a Fake API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
+import { useEffect, useReducer } from 'react';
 import Header from './Header';
 import Main from './Main';
 
+const initialState = {
+  questions: [],
+  status: 'loading', // 'loading', 'error', 'ready', 'active', 'finished'
+  currentQuestionIndex: 0,
+  userAnswers: [],
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Unknown action type');
+  }
+}
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch('http://localhost:3001/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  });
+
   return (
     <div className='app'>
       <Header />
