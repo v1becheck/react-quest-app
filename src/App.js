24bd481a4ef1e4737e92075b610484b5e50082e7
Milos Ilic <milosilic89@outlook.com>
2024-02-03 12:29:30 +0100

    Starting a New Quiz

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Loader from './Loader';
 import Error from './Error';
 import Main from './Main';
 import StartScreen from './StartScreen';
+import Question from './Question';
 
 const initialState = {
   questions: [],
@@ -18,6 +19,8 @@ function reducer(state, action) {
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
     default:
       throw new Error('Unknown action type');
   }
@@ -33,7 +36,7 @@ export default function App() {
       .then((res) => res.json())
       .then((data) => dispatch({ type: 'dataReceived', payload: data }))
       .catch((err) => dispatch({ type: 'dataFailed' }));
-  });
+  }, []);
 
   return (
     <div className='app'>
@@ -41,7 +44,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question />}
       </Main>
     </div>
   );
